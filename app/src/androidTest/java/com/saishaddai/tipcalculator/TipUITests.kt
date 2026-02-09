69539615cf15d6492036b71febd8ab9a6f412279
Saidel Lopez <sailopher@yahoo.com.mx>
2026-02-08 23:43:23 -0600

    implemented the instrumentation test

--- app/src/androidTest/java/com/saishaddai/tipcalculator/TipUITests.kt
+++ app/src/androidTest/java/com/saishaddai/tipcalculator/TipUITests.kt
@@ -1,9 +1,12 @@
 package com.saishaddai.tipcalculator
 
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
 import com.saishaddai.tipcalculator.ui.theme.TipTimeTheme
 import org.junit.Rule
 import org.junit.Test
+import java.text.NumberFormat
 
 class TipUITests {
 
@@ -17,6 +20,15 @@ class TipUITests {
                 TipTimeLayout()
             }
         }
+        composeTestRule
+            .onNodeWithText("Bill Amount")
+            .performTextInput("10")
+        composeTestRule
+            .onNodeWithText("Tip Percentage")
+            .performTextInput("20")
+        val expectedTip = NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists(
+            "No node with this text was found.")
     }
 
 }
